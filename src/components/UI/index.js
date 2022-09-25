1301c9ee80b9c0acb80fce212dac7114cf96ef8b
sheryak_n <nathanraysheryak@gmail.com>
2022-09-25 16:44:36 -0400

    create ui/index file for easier imports

---
+++ src/components/UI/index.js
@@ -0,0 +1,7 @@
+//////////////////////////////////////////////////////////////////////////////
+// Importing all UI components here allows for easier imports in components //
+// ex `import { Button, Input} fron "../../components/UI"`                  //
+//////////////////////////////////////////////////////////////////////////////
+
+export { default as Button } from "./Button/Button"
+export { default as Input } from "./Input/Input"
--- src/pages/auth/AuthPage.js
+++ src/pages/auth/AuthPage.js
@@ -1,3 +1,15 @@
+import { Button, Input } from "../../components/UI"
+
 export default function AuthPage() {
-  return <h1>I am AuthPage</h1>
+  return (
+    <div>
+      <h1>I am AuthPage</h1>
+
+      <form>
+        <Input label="username" placeholder="tsuki_cat" type="text" />
+        <Input label="password" placeholder="meow" type="password" />
+        <Button label="login" color="blue" />
+      </form>
+    </div>
+  )
 }
