0200da7761aae2e4a385007b873f231238f434a1
sheryak_n <nathanraysheryak@gmail.com>
2022-10-06 06:40:12 -0400

    hit api on load

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -6,28 +6,10 @@ export default function HomePage() {
   const [tableData, setTableData] = useState([])
 
   const fetchData = () => {
-    console.log("go get data!")
-    const dummyData = [
-      {
-        date: "2022-09-04",
-        id: 2,
-        user_id: 6,
-        weight: 7.9,
-      },
-      {
-        date: "2022-08-04",
-        id: 3,
-        user_id: 6,
-        weight: 18.7,
-      },
-      {
-        date: "2022-07-04",
-        id: 4,
-        user_id: 6,
-        weight: 6,
-      },
-    ]
-    setTableData(dummyData)
+    fetch("http://localhost:5000/weights/6")
+      .then((response) => response.json())
+      .then((data) => setTableData(data))
+      .catch((error) => console.log("error ", error))
   }
 
   useEffect(fetchData, [])
