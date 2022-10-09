acd520975275790fe9e9da95109a4dd5f7a3eba8
sheryak_n <nathanraysheryak@gmail.com>
2022-10-09 19:22:03 -0400

    delete weight

--- src/components/WeightTableRow/WeightTableRow.js
+++ src/components/WeightTableRow/WeightTableRow.js
@@ -5,12 +5,27 @@ import styles from "./WeightTableRow.module.css"
 function WeightTableRow(props) {
   const { date, id, userId, weight } = props
 
+  const deleteWeight = () => {
+    fetch("http://localhost:5000/weights/" + id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("data ", data))
+      .catch((error) => console.log("error ", error))
+  }
+
   return (
     <tr className={styles.row}>
       <td>{date}</td>
       <td>{id}</td>
       <td>{userId}</td>
       <td>{weight}</td>
+      <td>
+        <button onClick={deleteWeight}>x</button>
+      </td>
     </tr>
   )
 }
