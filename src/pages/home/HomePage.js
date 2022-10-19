5075bf6954f52b44798fa87b04c680bfbaa55fab
sheryak_n <nathanraysheryak@gmail.com>
2022-10-19 06:19:08 -0400

    use user.id to make onMount query

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+
+import AuthContext from "../../context/auth-context"
 
 import WeightTable from "../../components/WeightTable/WeightTable"
 
 export default function HomePage() {
   const [tableData, setTableData] = useState([])
+  const { user } = useContext(AuthContext)
 
   const fetchData = () => {
-    /**
-     * the userId is hardcoded right now - update later
-     */
-    fetch("http://localhost:5000/weights/6")
+    fetch("http://localhost:5000/weights/" + user.id)
       .then((response) => response.json())
       .then((data) => setTableData(data))
       .catch((error) => console.log("error ", error))
   }
 
-  useEffect(fetchData, [])
+  useEffect(fetchData, [user])
 
   return (
     <div>
