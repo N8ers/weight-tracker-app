cf81daf1660374fb5c4f5ce8c1bff77a8c8f8da8
sheryak_n <nathanraysheryak@gmail.com>
2022-10-20 06:39:33 -0400

    add form to upload csv w/ no network request

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -4,9 +4,21 @@ import AuthContext from "../../context/auth-context"
 
 import WeightTable from "../../components/WeightTable/WeightTable"
 
+import { Button } from "../../components/UI"
+
 export default function HomePage() {
   const [tableData, setTableData] = useState([])
+  const [isCSVUploaded, setIsCSVUploaded] = useState(false)
   const { user } = useContext(AuthContext)
+
+  const handleFileUpload = () => {
+    setIsCSVUploaded(true)
+  }
+
+  const uploadFile = () => {
+    // An endpoint is needed
+    console.log("GO UPLOAD!")
+  }
 
   const fetchData = () => {
     fetch("http://localhost:5000/weights/" + user.id)
@@ -20,6 +32,13 @@ export default function HomePage() {
   return (
     <div>
       <h1>I am the HomePage</h1>
+      <div>
+        <span>Upload CSV: </span>
+        <input type="file" onChange={handleFileUpload} />
+        {isCSVUploaded && (
+          <Button label="upload CSV" color="green" onClick={uploadFile} />
+        )}
+      </div>
       <WeightTable tableData={tableData} />
     </div>
   )
